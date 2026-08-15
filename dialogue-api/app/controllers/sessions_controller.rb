10d252cd37b6d7ca117734b312a664cf4fedc46e
graypegg <me@graypegg.com>
2026-08-15 12:26:07 -0400

    moved everything one directory deep so we can fit the front end in

--- dialogue-api/app/controllers/sessions_controller.rb
+++ dialogue-api/app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@ class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
 
-  def new
-  end
-
   def create
     if user = User.authenticate_by(params.permit(:user_name, :password))
       start_new_session_for user
